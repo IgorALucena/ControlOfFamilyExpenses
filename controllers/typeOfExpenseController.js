11ac241eef4181a5a81646ff2ea50a6bbb11d300
Igor <igoralbuquerque021@gmail.com>
2024-01-04 20:48:09 -0300

    feat: CRUD tipo_gasto

--- controllers/typeOfExpenseController.js
+++ controllers/typeOfExpenseController.js
@@ -57,4 +57,64 @@ let typeOfExpenseController = async (req, res) => {
 
 }
 
-module.exports = { typeOfExpenseController };
+let registerTypeOfExpenseController = async (req, res) => {
+    try {
+        let { nome } = req.body;
+        let result = await prisma.tipo_gasto.create({
+            data: {
+                nome: nome
+            }
+        })
+        res.status(200).json({
+            status: 'successfully registered',
+            data: result
+        })
+    }
+    catch (err) {
+        res.status(404).json({ msg: `Error: ${err}` });
+    }
+}
+
+let deleteTypeOfExpenseController = async (req, res) => {
+    try {
+        let id = parseInt(req.params.id);
+        let result = await prisma.tipo_gasto.delete({
+            where: {
+                id: id
+            }
+        })
+        res.status(200).json({
+            status: 'successfully deleted',
+            data: result
+        })
+
+    }
+    catch (err) {
+        res.status(404).json({ msg: `Error: ${err}` });
+    }
+}
+
+let editTypeOfExpenseController = async (req, res) => {
+    try {
+        let id = parseInt(req.params.id);
+        let { nome } = req.body;
+        let result = await prisma.tipo_gasto.update({
+            data:{
+                nome:nome
+            },
+            where: {
+                id:id
+            }
+        })
+        res.status(200).json({
+            status: 'successfully updated',
+            data: result
+        })
+
+    }
+    catch (err) {
+        res.status(404).json({ msg: `Error: ${err}` });
+    }
+}
+
+module.exports = { typeOfExpenseController, registerTypeOfExpenseController, deleteTypeOfExpenseController, editTypeOfExpenseController };
--- routes/routeTypeOfExpense.js
+++ routes/routeTypeOfExpense.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routerTypeOfExpense = express.Router();
-const { typeOfExpenseController } = require('../controllers/typeOfExpenseController');
+const { typeOfExpenseController, registerTypeOfExpenseController, deleteTypeOfExpenseController, editTypeOfExpenseController } = require('../controllers/typeOfExpenseController');
 
 routerTypeOfExpense.get('/:type?', typeOfExpenseController);
+
+routerTypeOfExpense.post('/register', registerTypeOfExpenseController);
+
+routerTypeOfExpense.delete('/delete/:id', deleteTypeOfExpenseController);
+
+routerTypeOfExpense.put('/edit/:id', editTypeOfExpenseController);
 
 module.exports = { routerTypeOfExpense };
